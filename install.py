09ae8445d4594a8b0d697a4adcf0195267567feb
Matěj Vrobel <matejvrobel@gmail.com>
2026-05-15 00:51:05 +0200

    remove autonomy

--- install.py
+++ install.py
@@ -34,7 +34,7 @@ DEFAULTS: dict[str, str] = {
     "WS_PATH": str(Path.home() / "rmodus_ws"),
     "DEPLOY_PATH": str(Path.home() / "rmodus_setup"),
     "FETCH_SW_NAV_MODULE": "1",
-    "SW_NAV_SPARSE_DIRS": "rmodus_hw rmodus_web rmodus_interface rmodus_description rmodus_autonomy",
+    "SW_NAV_SPARSE_DIRS": "rmodus_hw rmodus_web rmodus_interface rmodus_description", #rmodus_autonomy
     "SW_NAV_BRANCH": "main",
     "FETCH_XSENS_DRIVER": "0",
     "BUILD_XSPUBLIC": "1",
